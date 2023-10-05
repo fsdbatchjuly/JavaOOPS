0eca7c7c1155874884a406bd8836c66eeeabaa08
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2023-10-05 11:13:10 +0530

    js into

---
+++ Oct5_MyWebApplication/studentjs.js
@@ -0,0 +1,30 @@
+
+console.log("Hello Team this is first js file")
+
+// let, const, var
+
+num1 = 10;
+num2 = 15;
+sum = num1 + num2;
+console.log(sum);
+sum++;
+//+ , - , *, /, %
+
+studentName = "Hello I'm Nagarjuna";
+
+console.log("Legth of the string is ", studentName.length);
+
+// halfString = studentName.slice(10,19);
+// halfString = studentName.substring(10,19);
+
+// slice(start, end)
+// substring(start, end)
+// substr(start, length)
+halfString = studentName.substr(10,9);
+console.log("Legth of the string is ", studentName.toUpperCase());
+
+
+console.log("Half String value is ", halfString);
+
+
+
