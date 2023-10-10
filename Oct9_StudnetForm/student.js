163f1f1eea1c3db7a26d82c98d1f61d1c6afba7f
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2023-10-10 19:45:49 +0530

    API introduction

--- Oct9_StudnetForm/student.js
+++ Oct9_StudnetForm/student.js
@@ -1,35 +1,65 @@
 courseList = ['Java Full Stack',
-    'Python Full Stack',
-    'Web Development',
-    'Testing',
-    'Automation Testing',
-    'Artificial Intelligence',
-    'Informatica',
-    'Big Data',
-    'Tera Data',
-    'Robotics',
-    'Angular',
-    'React',
-    'Animation',
-    'SQl',
-    'NextJJS',
-    'Machine Learning',
-    'Oracle',
-    'C++',
-    'RUBY',
+'Python Full Stack',
+'Web Development',
+'Testing',
+'Automation Testing',
+'Artificial Intelligence',
+'Informatica',
+'Big Data',
+'Tera Data',
+'Robotics',
+'Angular',
+'React',
+'Animation',
+'SQl',
+'NextJJS',
+'Machine Learning',
+'Oracle',
+'C++',
+'RUBY',
 ];
-dropDownElement = document.getElementById("course")//<selectTag id="course"> </selectTag>
+// console.log("Script is executin");
+dropDownEle = document.getElementById("course")
 
-for (let i = 0; i < courseList.length; i++) {   
-    const optionElement = document.createElement("option");
-    optionElement.value = courseList[i].toLowerCase();
-    optionElement.text = courseList[i];
-    dropDownElement.appendChild(optionElement)    
+for(let i=0;i<courseList.length;i++){
+//   console.log(i,courseList[i]);
+optionElement = document.createElement("option");
+optionElement.value = courseList[i].toLowerCase();
+optionElement.text = courseList[i];
+dropDownEle.appendChild(optionElement);
 }
 
-function captureUserEnteredDetails(){
-    
-    nameInput = document.getElementById("name");
-    console.log(nameInput.value)
+function onSubmitClick(){
+nameEle = document.getElementById("name");
+console.log(nameEle.value);
 
+emailEle = document.getElementById("email");
+console.log(emailEle.value);
+
+ageEle = document.getElementById("age");
+console.log(ageEle.value);
 }
+
+
+// const apiUrl = 'http://localhost:8080/api/courses/getAllCourses';
+
+
+// // Use the fetch function to make a GET request
+// fetch(apiUrl)
+//   .then(response => {
+//     // Check if the response status code indicates success (e.g., 200 OK)
+//     if (!response.ok) {
+//       throw new Error(`HTTP error! Status: ${response.status}`);
+//     }
+//     // Parse the response body as JSON
+//     return response.json();
+//   })
+//   .then(data => {
+//     // Handle the JSON data from the API
+//     console.log(data);
+//     // You can update the DOM or perform other actions here
+//   })
+//   .catch(error => {
+//     // Handle any errors that occurred during the fetch
+//     console.error('Fetch error:', error);
+//   });
