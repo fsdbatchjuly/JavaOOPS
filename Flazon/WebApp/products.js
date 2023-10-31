2834f256c061322e367365a86626863769385ece
fsdbatchjuly <fsdbatchjuly2023@gmail.com>
2023-10-31 10:41:20 +0530

    display products list api integrated

--- Flazon/WebApp/products.js
+++ Flazon/WebApp/products.js
@@ -1,13 +1,49 @@
 fetch("http://localhost:8080/flazon_db/productsList")
     .then(response=>{
         if(!response.ok){
-            throw Error("something went wrog")
+            throw Error("something went wrong")
         }
         return response.json();
     })
     .then(data=>{
         console.log(data)
+        createProductsGrid(data);
     })
     .catch(error=>{
         console.log(error)
     })
+
+    function createProductsGrid(data){
+
+    //     <div class="product">
+    //     <img src="product1.jpg" alt="Product 1">
+    //     <h2>Product Name 1</h2>
+    //     <p>Category: Category 1</p>
+    //     <p>Cost: $19.99</p>
+    // </div>
+        gridElement = document.getElementById("product-grid")
+        for(let i=0;i<data.length;i++){
+            divEle = document.createElement("div")
+
+            imgEle = document.createElement("img")
+            imgEle.src = data[i].product_Image_url; 
+            imgEle.width = 150;       
+            imgEle.height = 150;
+
+            h2Elem = document.createElement("h2")
+            h2Elem.innerHTML = data[i].name
+
+            p1 = document.createElement("p")
+            p1.innerHTML = "Cost $" + data[i].cost;
+            
+            p2 = document.createElement("p")
+            p2.innerHTML = "Disount % " + data[i].discount;
+
+            divEle.appendChild(imgEle)
+            divEle.appendChild(h2Elem)
+            divEle.appendChild(p1)
+            divEle.appendChild(p2)
+
+            gridElement.appendChild(divEle)
+        }
+    }
